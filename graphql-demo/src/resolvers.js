89f90a9eceb514dd19f65f37825d57ab17f823b3
Juho Vepsalainen <bebraw@gmail.com>
2018-10-18 14:55:42 +0300

    feat: Persist data at the mutation

--- graphql-demo/src/resolvers.js
+++ graphql-demo/src/resolvers.js
@@ -1,16 +1,37 @@
-let helloState = 'world!'
+const fs = require('fs');
+const path = require('path');
+
+const DATA_PATH = path.join(__dirname, './data.json');
+
+
+
+let helloState = loadData(DATA_PATH, 'world!');
 
 const resolvers = {
   Mutation: {
     changeHello: (_, { newHello }) => {
-      helloState = newHello
+      helloState = newHello;
 
-      return helloState
+      saveData(DATA_PATH, helloState);
+
+      return helloState;
     }
   },
   Query: {
     hello: () => helloState
   },
 };
+
+function loadData(dataPath, initialData) {
+  if (fs.existsSync(dataPath)) {
+    return fs.readFileSync(dataPath, 'utf8')
+  }
+
+  return initialData;
+}
+
+function saveData(dataPath, data) {
+  fs.writeFileSync(dataPath, data);
+}
 
 export default resolvers;
