c0412e813f5a13e264785215ae1ea36619af6255
Juho Vepsalainen <bebraw@gmail.com>
2018-10-18 15:05:14 +0300

    chore: Set up initial test plan

---
+++ graphql-demo/src/resolvers.spec.js
@@ -0,0 +1,11 @@
+describe("Query#hello", () => {
+	it("should return internal state", async () => {
+		expect(true).toEqual(false);
+	});
+});
+
+describe("Mutation#changeHello", () => {
+	it("should change internal state", async () => {
+		expect(true).toEqual(false);
+	});
+});
