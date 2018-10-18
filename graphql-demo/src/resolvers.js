c548ec46a09c188caf7ff82d183870f59913485d
Juho Vepsalainen <bebraw@gmail.com>
2018-10-18 14:58:21 +0300

    chore: Format code

--- graphql-demo/src/resolvers.js
+++ graphql-demo/src/resolvers.js
@@ -1,34 +1,34 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const DATA_PATH = path.join(__dirname, './data.json');
-let helloState = loadData(DATA_PATH, 'world!');
+const DATA_PATH = path.join(__dirname, "./data.json");
+let helloState = loadData(DATA_PATH, "world!");
 
 const resolvers = {
-  Mutation: {
-    changeHello: (_, { newHello }) => {
-        helloState = newHello;
+	Mutation: {
+		changeHello: (_, { newHello }) => {
+			helloState = newHello;
 
-      saveData(DATA_PATH, helloState);
+			saveData(DATA_PATH, helloState);
 
-      return helloState;
-    }
-  },
-  Query: {
-    hello: () => helloState
-  },
+			return helloState;
+		}
+	},
+	Query: {
+		hello: () => helloState
+	}
 };
 
 function loadData(dataPath, initialData) {
-  if (fs.existsSync(dataPath)) {
-    return fs.readFileSync(dataPath, 'utf8')
-  }
+	if (fs.existsSync(dataPath)) {
+		return fs.readFileSync(dataPath, "utf8");
+	}
 
-  return initialData;
+	return initialData;
 }
 
 function saveData(dataPath, data) {
-  fs.writeFileSync(dataPath, data);
+	fs.writeFileSync(dataPath, data);
 }
 
 export default resolvers;
