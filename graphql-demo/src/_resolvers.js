450e007c882efdf301adec8f8995cecd1394589c
Juho Vepsalainen <bebraw@gmail.com>
2018-10-18 16:40:16 +0300

    chore: Sketch out image uploads

--- graphql-demo/src/_resolvers.js
+++ graphql-demo/src/_resolvers.js
@@ -1,9 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+const mkdirp = require("mkdirp");
+const cuid = require("cuid");
+
+const IMAGE_PATH = path.join(__dirname, "uploads");
+
+mkdirp.sync(IMAGE_PATH);
+
 module.exports = ({ get, set }) => ({
 	Mutation: {
 		changeHello: (_, { newHello }) => {
 			set("hello", newHello);
 
 			return newHello;
+		},
+		uploadImage: (_, { newImage }) => {
+			const buffer = Buffer.from(newImage, "base64");
+			const imagePath = path.join(IMAGE_PATH, "dummy.png");
+			const imageId = cuid();
+
+			fs.writeFileSync(imagePath, buffer, "binary");
+
+			return imageId;
 		}
 	},
 	Query: {
